cdb091f55574c06022eff38dfa3308efeb727385
Nasahahapatimal <dusan.blagojevic1990@gmail.com>
2023-06-27 21:53:18 +0200

    Fixed method add_to_basket()

--- Classes.py
+++ Classes.py
@@ -42,23 +42,30 @@ class Gluvo:
             return ("Invalid password")
         return ("Invalid username")
 
-    def add_to_basket(self, clan, password,  nazivi_jela, dostavljen):
-        if self.log_in(clan, password) == "Kupac":
-            if clan not in self.porudzbina_df["user_kupca"].to_list():
-                cursor = self.con.cursor()
-                obrok = "INSERT INTO PORUDZBINA(USER_KUPCA,NAZIVI_JELA,DOSTAVLJEN) VALUES ('{}',ARRAY{},'{}')".format(
-                    clan, nazivi_jela, dostavljen)
-                cursor.execute(obrok)
-                self.con.commit()
-                cursor.close()
-            else:
-                cursor = self.con.cursor()
-                obrok = "UPDATE PORUDZBINA SET {} = '{}' WHERE NAZIVI_JELA = ARRAY{}".format(
-                    nazivi_jela)
-                # STIGAO SI DO OVDE
-                cursor.execute(obrok)
-                self.con.commit()
-                cursor.close()
+    def add_to_basket(self, clan, password, nazivi_jela, kolicina, dostavljen = False):
+        
+        filter_df = self.porudzbina_df[self.porudzbina_df["user_kupca"].str.contains(clan)]
+
+        if self.log_in(clan, password) == "Kupac":#mozda i ne mora...OVO IDE U TKINTER.
+                if any(nazivi_jela[0] in item for item in filter_df["nazivi_jela"]):
+                    return "Jelo je vec na spisku"
+                if clan not in filter_df["user_kupca"].to_list():
+                    cursor = self.con.cursor()
+                    obrok = "INSERT INTO PORUDZBINA(USER_KUPCA,NAZIVI_JELA,KOLICINA,DOSTAVLJEN) VALUES ('{}',ARRAY{},ARRAY{},'{}')".format(
+                        clan, nazivi_jela, kolicina,dostavljen)
+                    cursor.execute(obrok)
+                    self.con.commit()
+                    cursor.close()
+                else:
+                    cursor = self.con.cursor()
+                    novo_jelo = "UPDATE PORUDZBINA SET NAZIVI_JELA = NAZIVI_JELA || ARRAY{} WHERE USER_KUPCA = '{}'".format(
+                    nazivi_jela, clan)
+                    nova_kolicina = "UPDATE PORUDZBINA SET KOLICINA = KOLICINA || ARRAY{} WHERE USER_KUPCA = '{}'".format(
+                    kolicina, clan)
+                    cursor.execute(novo_jelo)
+                    cursor.execute(nova_kolicina)
+                    self.con.commit()
+                    cursor.close()
 
     def in_the_bakset(self):
         pass
@@ -80,5 +87,5 @@ dostavljanje.import_sql()
 # korisnik = dostavljanje.log_in("Punker", "1234")
 # print(dostavljanje.jela_df["naziv"])
 # print(dostavljanje.restoran_df)
-print(dostavljanje.add_to_basket("Punker", "1234", ["Kupus"], False))
+print(dostavljanje.add_to_basket("Lizi", "1234", ["Palacinke"],["2"]))
 # print(dostavljanje.korisnik_df.tip_korisnika)
